6de077103c179f69adcc31e8d465ecdc44649db2
nico.vanelslande <nico.vanelslande@red-gate.com>
2015-09-29 15:44:20 +0100

    standalone example scripts to use hardcoded default version numbers
    for nunit and dotcover

--- scripts/test-singleassembly.ps1
+++ scripts/test-singleassembly.ps1
@@ -26,13 +26,13 @@ param(
     [Parameter(Mandatory=$true)]
     [string] $AssemblyPath,
     # The version of the nuget package containing the NUnit executables (NUnit.Runners)
-    [string] $NUnitVersion = $DefaultNUnitVersion,
+    [string] $NUnitVersion = '2.6.4',
     [switch] $Nunitx86,
     [string[]] $ExcludedCategories = @(),
     [string[]] $IncludedCategories = @(),
     [bool] $EnableCodeCoverage = $false,
     # The version of the nuget package containing DotCover.exe (JetBrains.dotCover.CommandLineTools)
-    [string] $DotCoverVersion = $DefaultDotCoverVersion,
+    [string] $DotCoverVersion = '3.2.0',
     [string] $DotCoverFilters = ''
 )
 
