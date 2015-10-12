34dc7008f22579f7b365b524c942dcf668e210a0
nico.vanelslande <nico.vanelslande@red-gate.com>
2015-10-12 14:35:08 +0100

    Install-SmartAssemblyPackage does not deal with SmartAssembly.settings anymore.
    SmartAssembly.settings should be managed outside of RedGate.Build
    (puppet on build agents, manually/scripted for developers?)

--- Public/Install-SmartAssemblyPackage.ps1
+++ Public/Install-SmartAssemblyPackage.ps1
@@ -9,16 +9,5 @@ function Install-SmartAssemblyPackage {
     [string] $SmartAssemblyVersion = $DefaultSmartAssemblyVersion
   )
 
-  $SAFolder = Install-Package SmartAssembly $SmartAssemblyVersion
-
-  if($env:TEAMCITY_VERSION -eq $null) {
-    # Running on a dev machine, if the package contains a SmartAssembly.settings file, ignore it (by deleting it)
-    #   (because SmartAssembly.settings Options could be set to connet to a central SQL database.
-    #   and we don't want to pollute it with developer build data.)
-    if(Test-Path "$SAFolder\tools\SmartAssembly.settings" ) {
-      Remove-Item "$SAFolder\tools\SmartAssembly.settings" -Force | Out-Null
-    }
-  }
-
-  $SAFolder
+  Install-Package SmartAssembly $SmartAssemblyVersion
 }
