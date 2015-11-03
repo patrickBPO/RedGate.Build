f102e6a2e0e4747aaee3a23a886480578beea9e1
Chris Lambrou <github@chrislambrou.com>
2015-11-03 14:58:30 +0000

    Ensure Pester fails when any tests fail.

--- build.ps1
+++ build.ps1
@@ -111,7 +111,7 @@ try {
   
   # Run Pester tests.
   Write-Info 'Running Pester tests'
-  Invoke-Pester -Script .\Tests\*.Tests.ps1 -OutputFile $PesterResultsPath -OutputFormat NUnitXml
+  Invoke-Pester -Script .\Tests\*.Tests.ps1 -OutputFile $PesterResultsPath -OutputFormat NUnitXml -EnableExit
   if($LASTEXITCODE -ne 0) {
     throw 'One or more tests failed.'
   }
