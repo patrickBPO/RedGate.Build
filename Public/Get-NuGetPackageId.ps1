e87bcc4df891cf01f565783699fed5c17f9eff49
Chris Lambrou <github@chrislambrou.com>
2015-11-06 15:53:24 +0000

    Simplify and remove use of env variables.

--- Public/Get-NuGetPackageId.ps1
+++ Public/Get-NuGetPackageId.ps1
@@ -4,36 +4,35 @@ function Get-NuGetPackageId
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
-        [version] $Version
+        [version] $Version,
+
+        [Parameter(Mandatory = $false)]
+        [string] $BranchName,
+
+        [Parameter(Mandatory = $false)]
+        [switch] $IsDefaultBranch
     )
 
-    # Establish the basic pre-release suffix.
-    $PreReleaseSuffix = '-dev' # For dev builds, always use the '-dev' prerelease suffix.
-    if ($env:BRANCH_NAME) 
-    {
-        # For build server builds, use the branch name, or no suffix on the default branch.
-        if ($env:IS_DEFAULT_BRANCH -eq 'True') 
-        {
-            $PreReleaseSuffix = ''
-        }
-        else 
-        {
-            $PreReleaseSuffix = "-$env:BRANCH_NAME" -replace '[^0-9A-Za-z-]', ''
-        }
-    }
-
-    # If there's no pre-release suffix (because we're on the master branch on the build server), just return the version.
-    if ($PreReleaseSuffix -eq '') 
+    # If this is the default branch, there's no pre-release suffix. Just return the version number.
+    if ($IsDefaultBranch.IsPresent)
     {
         return [string]$Version
     }
 
-    # Shorten the pre-release suffix if necessary, to satisfy NuGet's 20 character limit.
+    # Otherwise establish the pre-release suffix from the branch name.
+    $PreReleaseSuffix = "-$BranchName"
+
+    # Remove invalid characters from the suffix.
+    $PreReleaseSuffix = $PreReleaseSuffix -replace '[^0-9A-Za-z-]', ''
+
+    # Shorten the suffix if necessary, to satisfy NuGet's 20 character limit.
     $Revision = [string]$Version.Revision
     $MaxLength = 20 - $Revision.Length
     if ($PreReleaseSuffix.Length -gt $MaxLength) 
     {
         $PreReleaseSuffix = $PreReleaseSuffix -replace '[aeiou]', ''
+
+        # If the suffix is still too long after we've stripped out the vovels, truncate it.
         if ($PreReleaseSuffix.Length -gt $MaxLength) 
         {
             $PreReleaseSuffix = $PreReleaseSuffix.Substring(0, $MaxLength)
