62d2af872089eda2f19df8c000dd2b7874c0d306
nico.vanelslande <nico.vanelslande@red-gate.com>
2015-11-19 16:55:47 +0000

    Invoke-SigningService fix: we do need a process { } block in order to pipe files in.

--- Public/Invoke-SigningService.ps1
+++ Public/Invoke-SigningService.ps1
@@ -1,15 +1,15 @@
 function Add-ToHashTableIfNotNull {
-  param(
-    [Parameter(Mandatory=$true)]
-    [HashTable] $HashTable,
-    [Parameter(Mandatory=$true)]
-    [string] $Key,
-    [string] $Value
-  )
+    param(
+        [Parameter(Mandatory=$true)]
+        [HashTable] $HashTable,
+        [Parameter(Mandatory=$true)]
+        [string] $Key,
+        [string] $Value
+    )
 
-  if( $Value ) {
-    $HashTable.Add($Key, $Value)
-  }
+    if( $Value ) {
+        $HashTable.Add($Key, $Value)
+    }
 }
 
 <#
@@ -52,63 +52,65 @@ function Add-ToHashTableIfNotNull {
     This shows how to sign a Visual Studio Installer file. The signing service URL is taken from the $env:SigningServiceUrl environment variable that is present on all of the build agents.
 #>
 function Invoke-SigningService {
-  [CmdletBinding()]
-  param(
-    [Parameter(Mandatory = $True, ValueFromPipeline = $True)]
-    [Alias('JarPath', 'VsixPath', 'ClickOnceApplicationPath', 'AssemblyPath')]
-    [string] $FilePath,
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $True, ValueFromPipeline = $True)]
+        [Alias('JarPath', 'VsixPath', 'ClickOnceApplicationPath', 'AssemblyPath')]
+        [string] $FilePath,
 
-    [Parameter(Mandatory = $False)]
-    [string] $SigningServiceUrl = $env:SigningServiceUrl,
+        [Parameter(Mandatory = $False)]
+        [string] $SigningServiceUrl = $env:SigningServiceUrl,
 
-    [Parameter(Mandatory = $False)]
-    [string] $Certificate = 'Master',
+        [Parameter(Mandatory = $False)]
+        [string] $Certificate = 'Master',
 
-    [Parameter(Mandatory = $False)]
-    [string] $Description = 'Red Gate Software Ltd.',
+        [Parameter(Mandatory = $False)]
+        [string] $Description = 'Red Gate Software Ltd.',
 
-    [Parameter(Mandatory = $False)]
-    [string] $MoreInfoUrl = 'http://www.red-gate.com'
-  )
+        [Parameter(Mandatory = $False)]
+        [string] $MoreInfoUrl = 'http://www.red-gate.com'
+    )
 
-  # Simple error checking.
-  if ([String]::IsNullOrEmpty($SigningServiceUrl)) {
-    throw 'Cannot sign assembly. -SigningServiceUrl was not specified and the SigningServiceUrl environment variable is not set.'
-  }
-  if (!(Test-Path $FilePath)) {
-    throw "File not found: $FilePath"
-  }
+    process {
+        # Simple error checking.
+        if ([String]::IsNullOrEmpty($SigningServiceUrl)) {
+            throw 'Cannot sign assembly. -SigningServiceUrl was not specified and the SigningServiceUrl environment variable is not set.'
+        }
+        if (!(Test-Path $FilePath)) {
+            throw "File not found: $FilePath"
+        }
 
-  # Determine the file type.
-  $FileType = $Null
-  switch ([System.IO.Path]::GetExtension($FilePath)) {
-    '.exe' { $FileType = 'Exe' }
-    '.dll' { $FileType = 'Exe' }
-    '.vsix' { $FileType = 'Vsix' }
-    '.jar' { $FileType = 'Jar' }
-    '.application' { $FileType = 'ClickOnce' }
-    default { throw "Unsupported file type: $AssemblyPath" }
-  } 
+        # Determine the file type.
+        $FileType = $Null
+        switch ([System.IO.Path]::GetExtension($FilePath)) {
+            '.exe' { $FileType = 'Exe' }
+            '.dll' { $FileType = 'Exe' }
+            '.vsix' { $FileType = 'Vsix' }
+            '.jar' { $FileType = 'Jar' }
+            '.application' { $FileType = 'ClickOnce' }
+            default { throw "Unsupported file type: $AssemblyPath" }
+        }
 
-  # Make the web request to the signing service.
-  $Headers = @{};
-  Add-ToHashTableIfNotNull $Headers -Key 'FileType' -Value $FileType
-  Add-ToHashTableIfNotNull $Headers -Key 'Certificate' -Value $Certificate
-  Add-ToHashTableIfNotNull $Headers -Key 'Description' -Value $Description
-  Add-ToHashTableIfNotNull $Headers -Key 'MoreInfoUrl' -Value $MoreInfoUrl
+        # Make the web request to the signing service.
+        $Headers = @{};
+        Add-ToHashTableIfNotNull $Headers -Key 'FileType' -Value $FileType
+        Add-ToHashTableIfNotNull $Headers -Key 'Certificate' -Value $Certificate
+        Add-ToHashTableIfNotNull $Headers -Key 'Description' -Value $Description
+        Add-ToHashTableIfNotNull $Headers -Key 'MoreInfoUrl' -Value $MoreInfoUrl
 
-  Write-Verbose "Signing $FilePath using $SigningServiceUrl"
-  $Headers.Keys | ForEach { Write-Verbose "`t $_`: $($Headers[$_])" }
+        Write-Verbose "Signing $FilePath using $SigningServiceUrl"
+        $Headers.Keys | ForEach { Write-Verbose "`t $_`: $($Headers[$_])" }
 
-  $Response = Invoke-WebRequest `
-    -Uri $SigningServiceUrl `
-    -InFile $FilePath `
-    -OutFile $FilePath `
-    -Method Post `
-    -ContentType 'binary/octet-stream' `
-    -Headers $Headers
+        $Response = Invoke-WebRequest `
+            -Uri $SigningServiceUrl `
+            -InFile $FilePath `
+            -OutFile $FilePath `
+            -Method Post `
+            -ContentType 'binary/octet-stream' `
+            -Headers $Headers
 
-  # TODO: How should we check the response? Need to fail if the signing failed.
+        # TODO: How should we check the response? Need to fail if the signing failed.
 
-  return $FilePath
+        return $FilePath
+    }
 }
