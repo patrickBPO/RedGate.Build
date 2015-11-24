3c21bb723bccdc55bbcfc32cbf771ead8dfdbedc
Chris Lambrou <github@chrislambrou.com>
2015-11-24 08:08:41 +0000

    Missed changes from previous commit.

--- Tests/Get-NuGetPackageVersion.Tests.ps1
+++ Tests/Get-NuGetPackageVersion.Tests.ps1
@@ -3,21 +3,21 @@
 Describe 'Get-NuGetPackageVersion' {
     Context 'When IsDefaultBranch is true' {
         It 'should return the Version' {
-            Get-NuGetPackageId -Version '1.2.3.4' -IsDefaultBranch $True -BranchName 'master' | Should Be '1.2.3.4'
+            Get-NuGetPackageVersion -Version '1.2.3.4' -IsDefaultBranch $True -BranchName 'master' | Should Be '1.2.3.4'
         }
     }
     Context 'When IsDefaultBranch is false' {
         it 'should throw when BranchName is empty' {
-            { Get-NuGetPackageId -Version '1.2.3.4' -IsDefaultBranch $False -BranchName '' } | Should Throw
+            { Get-NuGetPackageVersion -Version '1.2.3.4' -IsDefaultBranch $False -BranchName '' } | Should Throw
         }
         it 'should use the BranchName and revision number as the pre-release suffix' {
-            Get-NuGetPackageId -Version '1.2.3.4' -IsDefaultBranch $False -BranchName 'SomeBranch' | Should Be '1.2.3-SomeBranch4'
+            Get-NuGetPackageVersion -Version '1.2.3.4' -IsDefaultBranch $False -BranchName 'SomeBranch' | Should Be '1.2.3-SomeBranch4'
         }
         it 'should shorten the pre-release suffix (by removing vowels) if the BranchName is too long' {
-            Get-NuGetPackageId -Version '1.2.3.4' -IsDefaultBranch $False -BranchName 'SomeBranchNameThatsTooLong' | Should Be '1.2.3-SmBrnchNmThtsTLng4'
+            Get-NuGetPackageVersion -Version '1.2.3.4' -IsDefaultBranch $False -BranchName 'SomeBranchNameThatsTooLong' | Should Be '1.2.3-SmBrnchNmThtsTLng4'
         }
         it 'should shorten the pre-release suffix (by removing vowels and then truncating) if the BranchName is too long' {
-            Get-NuGetPackageId -Version '1.2.3.4' -IsDefaultBranch $False -BranchName 'SomeBranchNameThatsReallyFarTooLong' | Should Be '1.2.3-SmBrnchNmThtsRllyF4'
+            Get-NuGetPackageVersion -Version '1.2.3.4' -IsDefaultBranch $False -BranchName 'SomeBranchNameThatsReallyFarTooLong' | Should Be '1.2.3-SmBrnchNmThtsRllyF4'
         }
     }
 }
