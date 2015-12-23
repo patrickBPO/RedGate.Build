8033afa3828f9887efa9a90489beb6f4cc9c4078
nico.vanelslande <nico.vanelslande@red-gate.com>
2015-12-23 14:48:32 +0000

    Always export all aliases.

--- RedGate.Build.psm1
+++ RedGate.Build.psm1
@@ -21,12 +21,16 @@ $teamcityModule = Import-Module $PSScriptRoot\Private\teamcity.psm1 -DisableName
     }
 
 
+
 if ($Host.Name -ne "Default Host") {
   Write-Host "RedGate.Build is using its own nuget.exe. Version $((Get-Item $nugetExe).VersionInfo.FileVersion)"
 }
 
+# Export all the functions from the Teamcity module
 Get-Command -Module $teamcityModule -CommandType Function | Export-ModuleMember
-Get-Command -Module $teamcityModule -CommandType Alias | ForEach { Export-ModuleMember -Alias $_ }
+
+# Always export all aliases.
+Export-ModuleMember -Alias *
 
 # For debug purposes, uncomment this to export all functions of this module.
 # Export-ModuleMember -Function *
