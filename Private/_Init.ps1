e40dea0cab13397f1304d30ec7b74b289cf704f5
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-01-11 12:49:32 +0000

    Redefine Write-Host when $Host.Name is 'Default Host'
    as per https://github.com/nightroman/Invoke-Build/wiki/Parallel-Builds#avoid-host-cmdlets-and-ui-members

--- Private/_Init.ps1
+++ Private/_Init.ps1
@@ -11,3 +11,17 @@ $NugetExe = Resolve-Path "$PSScriptRoot\nuget.exe"
 $DefaultNUnitVersion = '2.6.4'
 $DefaultDotCoverVersion = '3.2.0'
 $DefaultSmartAssemblyVersion = '6.8.0.248'
+
+
+if ($Host.Name -eq "Default Host") {
+    # Redefine Write-Host to avoid this error on Teamcity:
+    # 'A command that prompts the user failed because the host program or the command type does not support user interaction'
+    # This is only likely to happen when calling RedGate.Build functions from some Invoke-Build parallel tasks...
+    # More info at: https://github.com/nightroman/Invoke-Build/wiki/Parallel-Builds#avoid-host-cmdlets-and-ui-members
+    function global:Redirect-HostToOutput {
+        # Write to the output stream instead because we still want to be able to see teamcity publish messages...
+        Write-Output "$Args"
+    }
+    # Create a Write-Host alias to override the default Write-Host command.
+    New-Alias -Name Write-Host -Value Redirect-HostToOutput -Force -Scope Global
+}
