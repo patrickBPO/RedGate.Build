5d192eb538a366e02cc68f879c799338f43e2deb
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-02-04 15:12:42 +0000

    quick comment fix.

--- Public/Invoke-NUnitForAssembly.ps1
+++ Public/Invoke-NUnitForAssembly.ps1
@@ -88,7 +88,8 @@ function Invoke-NUnitForAssembly {
 
     # Tell teamcity to keep our test output logs as well. This could come in handy
     $assemblyFilename = Split-Path $AssemblyPath -Leaf
-    # $AssemblyPath.$TestResultFilenamePattern.*x* ? That's a horrible to only get the .xml and .txt file in 1 line.
+    # $AssemblyPath.$TestResultFilenamePattern.*x* ? That's a horrible way to only get the .xml and .txt file in 1 line.
+    # (and skip the *.coverage.snap file if any)
     # Teamcity doesn't seem to let you add files to an existing zipped file.... oh well...
     TeamCity-PublishArtifact "$AssemblyPath.$TestResultFilenamePattern.*x* => logs/tests/$assemblyFilename.$TestResultFilenamePattern/logs.zip"
   }
