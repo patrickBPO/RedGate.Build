41d061d8d21029e1dbcde40bf4cad21c34f068cf
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-02-05 15:18:44 +0000

    Verbose logging for Get-ProjectTargetFramework

--- Public/VisualStudio/Get-ProjectTargetFramework.ps1
+++ Public/VisualStudio/Get-ProjectTargetFramework.ps1
@@ -6,10 +6,12 @@
     Open the project file, read and return the value of TargetFrameworkVersion
 #>
 function Get-ProjectTargetFramework {
+    [CmdletBinding()]
     param(
         # The path to a .csproj or .vbproj file. (msbuild format)
         [string] $ProjectFile
     )
 
+    Write-Verbose "Project: $ProjectFile. Retrieving TargetFrameworkVersion"
     (([xml](Get-Content $ProjectFile)).project.propertygroup.TargetFrameworkVersion | select -first 1).tostring()
 }
