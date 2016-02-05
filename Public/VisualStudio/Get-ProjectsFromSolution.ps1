c9fb5398ff9c4e57b6d36eab2e2392eacc9d65d7
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-02-05 13:25:45 +0000

    A quick function to retrieve the list of project files from a Visual Studio solution file.

---
+++ Public/VisualStudio/Get-ProjectsFromSolution.ps1
@@ -0,0 +1,50 @@
+<#
+.SYNOPSIS
+    List the projects from a Visual Studio solution file
+
+.DESCRIPTION
+    List all the projects from a Visual Studio solution file
+    as well as display some of their property. (TargetFrameworkVersion)
+#>
+function Get-ProjectsFromSolution
+{
+    [CmdletBinding()]
+    Param
+    (
+        # The path to a Visual Studio solution file (.sln)
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        $SolutionFile
+    )
+    Process
+    {
+        # Crude parameter checking
+        $SolutionFile = Resolve-Path $SolutionFile
+
+        # Project files are relative to the solution directory.
+        # So get in the directory to be able to resolve project files path.
+        Split-Path $SolutionFile | Push-Location
+        try {
+
+            # Trust me, this gets .[cs|vb]proj files referenced in $SolutionFile
+            (
+                ((Get-Content $SolutionFile) -like 'Project(*') | ForEach { ($_ -split ',')[1] }
+            ) -like '*.*proj*' |
+                ForEach {
+                    $filepath = $_.Trim(' "')
+                    [pscustomobject] @{
+                        Project = $filepath
+                        TargetFrameworkVersion = (Get-ProjectTargetFramework $filepath)
+                    }
+                }
+        } finally {
+            Pop-Location
+        }
+    }
+}
+
+function Get-ProjectTargetFramework {
+    param( [string] $ProjectFile )
+
+    (([xml](Get-Content $ProjectFile)).project.propertygroup.TargetFrameworkVersion | select -first 1).tostring()
+}
