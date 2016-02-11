9eae9f35c2c2f1e76d9357d9a251dec525d9c99b
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-02-11 10:43:33 +0000

    Merge-CoverageReports: Fixed path to merged snapshot file when generating the html report.

--- Public/Merge-CoverageReports.ps1
+++ Public/Merge-CoverageReports.ps1
@@ -29,7 +29,7 @@ function Merge-CoverageReports {
 
   if( $env:TEAMCITY_VERSION -eq $null ) {
     # Create an HTML report if running outside of Teamcity to help with debugging
-    & $DotCoverPath report /Source="$MergedSnapshotPath.zip" /Output="$OutputDir\report.html" /reporttype=HTML
+    & $DotCoverPath report /Source="$MergedSnapshotPath" /Output="$OutputDir\report.html" /reporttype=HTML
   } else {
     # Let Teamcity know where the current dotcover.exe we are using is
     TeamCity-ConfigureDotNetCoverage -key 'dotcover_home' -value ($DotCoverPath | Split-Path)
