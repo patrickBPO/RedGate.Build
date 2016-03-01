14fb811100a5bcf18bb35413c3b4d4e6bcc91a21
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-03-01 10:19:11 +0000

    New-NuGetPackageVersion: do not remove vowels when truncating. Just truncate.
    + stick with the 4 part version number and stay away from Semver 1.0 for now.
     * 1.2.3.4
     * 1.2.3.4-prerelease-string
    instead of
     * 1.2.3.4
     * 1.2.3-prerelease-string4
    
    We can revisit this when nuget.exe and nuget server start supporting Semver 2.0

--- Tests/New-NuGetPackageVersion.Tests.ps1
+++ Tests/New-NuGetPackageVersion.Tests.ps1
@@ -10,14 +10,17 @@ Describe 'New-NuGetPackageVersion' {
         it 'should throw when BranchName is empty' {
             { New-NuGetPackageVersion -Version '1.2.3.4' -IsDefaultBranch $False -BranchName '' } | Should Throw
         }
-        it 'should use the BranchName and revision number as the pre-release suffix' {
-            New-NuGetPackageVersion -Version '1.2.3.4' -IsDefaultBranch $False -BranchName 'SomeBranch' | Should Be '1.2.3-SomeBranch4'
+        it 'should use the BranchName without the revision number as the pre-release suffix' {
+            New-NuGetPackageVersion -Version '1.2.3.4' -IsDefaultBranch $False -BranchName 'SomeBranch' | Should Be '1.2.3.4-SomeBranch'
         }
-        it 'should shorten the pre-release suffix (by removing vowels) if the BranchName is too long' {
-            New-NuGetPackageVersion -Version '1.2.3.4' -IsDefaultBranch $False -BranchName 'SomeBranchNameThatsTooLong' | Should Be '1.2.3-SmBrnchNmThtsTLng4'
+        it 'should shorten the pre-release suffix (by truncating) if the BranchName is too long' {
+            New-NuGetPackageVersion -Version '1.2.3.4' -IsDefaultBranch $False -BranchName 'SomeBranchNameThatsTooLong' | Should Be '1.2.3.4-SomeBranchNameThats'
         }
-        it 'should shorten the pre-release suffix (by removing vowels and then truncating) if the BranchName is too long' {
-            New-NuGetPackageVersion -Version '1.2.3.4' -IsDefaultBranch $False -BranchName 'SomeBranchNameThatsReallyFarTooLong' | Should Be '1.2.3-SmBrnchNmThtsRllyF4'
+        it 'should replace "/" in the pre-release suffix by "-"' {
+            New-NuGetPackageVersion -Version '1.2.3.4' -IsDefaultBranch $False -BranchName 'build/fixing-it' | Should Be '1.2.3.4-build-fixing-it'
+        }
+        it 'should remove invalid characters from the pre-release suffix' {
+            New-NuGetPackageVersion -Version '1.2.3.4' -IsDefaultBranch $False -BranchName 'invalid\\$%£/chars;+=_' | Should Be '1.2.3.4-invalid-chars'
         }
     }
 }
