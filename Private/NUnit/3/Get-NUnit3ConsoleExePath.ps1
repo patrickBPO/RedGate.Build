478d7f31a8b663c6abbd4579ea0077e7160776d4
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-07-07 11:49:37 +0100

    Adding tests for Get-NUnit3ConsoleExePath and locking it against Nunit v3 only.

--- Private/NUnit/3/Get-NUnit3ConsoleExePath.ps1
+++ Private/NUnit/3/Get-NUnit3ConsoleExePath.ps1
@@ -4,9 +4,13 @@ function Get-NUnit3ConsoleExePath {
         # The version of the nuget package containing the NUnit executables (NUnit.Console)
         [string] $NUnitVersion
     )
-    
+
+    if(!$NunitVersion.StartsWith('3.')) {
+        throw "Unexpected NUnit version '$NUnitVersion'. This function only supports Nunit v3"
+    }
+
     Write-Verbose "Using NUnit version $NUnitVersion"
     $NUnitFolder = Install-NUnitPackage $NUnitVersion
 
-    "$NUnitFolder\tools\nunit3.console.exe" | Resolve-Path
+    "$NUnitFolder\tools\nunit3-console.exe" | Resolve-Path
 }
